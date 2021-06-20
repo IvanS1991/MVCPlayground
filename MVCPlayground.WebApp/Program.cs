8a796892336ff4aff124b1b5b649458e49a49fd0
Ivan <haifischy91@gmail.com>
2021-06-20 16:44:54 +0300

    Add MVCPlayground.WebApp project with initial routes

---
+++ MVCPlayground.WebApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MVCPlayground.WebApp
+{
+    using System.Threading.Tasks;
+    using MVCPlayground.Framework.Http;
+    using MVCPlayground.Framework.Http.Response.Content;
+    using MVCPlayground.Framework.Http.Routing;
+    using MVCPlayground.Framework.Logging;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new HttpServer();
+
+            await server
+                .WithLogging(new ConsoleLogger())
+                .WithRouting(new HttpRoutingMap(), r => r
+                    .MapGet("/", req => new HtmlResponse("<h1>Works!</h1"))
+                    .MapGet("/Cats", req => new HtmlResponse("<h1>Cats</h1>"))
+                )
+                .Start();
+        }
+    }
+}
