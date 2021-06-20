e3ecbe113e1f7f996a804c4376868ed4e3b2cf28
Ivan <haifischy91@gmail.com>
2021-06-20 18:20:49 +0300

    Add basic abstract Controller

---
+++ MVCPlayground.Framework/Http/Controllers/Controller.cs
@@ -0,0 +1,36 @@
+﻿using MVCPlayground.Framework.Http.Request;
+using MVCPlayground.Framework.Http.Response;
+using MVCPlayground.Framework.Http.Response.Content;
+
+namespace MVCPlayground.Framework.Http.Controllers
+{
+    public abstract class Controller
+    {
+        public Controller(HttpRequest request)
+        {
+            this.Request = request;
+        }
+
+        protected HttpRequest Request { get; }
+
+        protected HttpResponse View(string html)
+        {
+            return new HtmlResponse(html);
+        }
+
+        protected HttpResponse Text(string text)
+        {
+            return new TextResponse(text);
+        }
+
+        protected HttpResponse NotFound()
+        {
+            return new NotFoundResponse();
+        }
+
+        protected HttpResponse Redirect(string url)
+        {
+            return new RedirectResponse(url);
+        }
+    }
+}
