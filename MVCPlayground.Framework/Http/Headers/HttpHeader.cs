bfb4bab25008e4ca8cb2944f7b3eb4ff1d7a1edf
Ivan <haifischy91@gmail.com>
2021-06-20 16:43:33 +0300

    Add HttpHeaderCollection

---
+++ MVCPlayground.Framework/Http/Headers/HttpHeader.cs
@@ -0,0 +1,15 @@
+﻿namespace MVCPlayground.Framework.Http.Headers
+{
+    using MVCPlayground.Framework.Common;
+
+    public class HttpHeader : NameValuePair
+    {
+        public HttpHeader(string name, string value)
+            : base(name, value) { }
+
+        public override string ToString()
+        {
+            return $"{this.Name}: {this.Value}";
+        }
+    }
+}
---
+++ MVCPlayground.Framework/Http/Headers/HttpHeaderCollection.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace MVCPlayground.Framework.Http.Headers
+{
+    using MVCPlayground.Framework.Common;
+    using MVCPlayground.Framework.Contracts;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HttpHeaderCollection : INameValuePairCollection<HttpHeader>, IEnumerable<HttpHeader>
+    {
+        private List<HttpHeader> headers;
+
+        public HttpHeaderCollection()
+        {
+            this.headers = new List<HttpHeader>();
+        }
+
+        public void Add(HttpHeader header)
+        {
+            this.headers.Add(header);
+        }
+
+        public bool Contains(string name)
+        {
+            Guard.AgainstNull(name, nameof(name));
+
+            return this.headers.Any(x => x.Name == name);
+        }
+
+        public HttpHeader Get(string name)
+        {
+            Guard.AgainstNull(name, nameof(name));
+
+            return this.headers.FirstOrDefault(x => x.Name == name);
+        }
+
+        public IEnumerator<HttpHeader> GetEnumerator()
+        {
+            return this.headers.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", this.Select(x => x.ToString()));
+        }
+    }
+}
